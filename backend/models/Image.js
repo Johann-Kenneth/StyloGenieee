a4cb173c130d8dbcfed5f957d28ed1857d7df79a
Johann-Kenneth <johann.ad.mfedu@gmail.com>
2024-12-22 18:18:20 +0530

    hehe

--- backend/models/Image.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const imageSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  imageData: { type: String, required: true }, 
-});
-module.exports = mongoose.model('Image', imageSchema);
--- backend/routes/chatbot.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const  {chatbot}  = require('../controllers/chatbotController');
-const router = express.Router();
-router.post('/chat', chatbot);
-module.exports = router;
-
